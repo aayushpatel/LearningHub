0a82c9c91abff7c219f1c12a070648514a452774
aayush <aayushp47@gmail.com>
2022-10-06 17:09:04 +0530

    userdir name change

--- src/test/java/aayushpatelframework/TestCases/StandaloneTest.java
+++ src/test/java/aayushpatelframework/TestCases/StandaloneTest.java
@@ -77,7 +77,7 @@ public class StandaloneTest extends Base {
 	@DataProvider
 	public Object[][] getdata() throws IOException {
 		List<HashMap<String, String>> data = getJsonMapData(
-				System.getProperty("user.dir") + "src\\test\\java\\aayushpatelframework\\data\\purchaseOrder.json");
+				System.getProperty("user.dir") + "\\src\\test\\java\\aayushpatelframework\\data\\purchaseOrder.json");
 		return new Object[][] { { data.get(0) }, { data.get(1) } };
 	}
 
